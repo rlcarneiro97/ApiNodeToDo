b8416b01d35b121f3272cf7a2c293388837b0b8b
rlcarneiro97 <rafaeldelima97@hotmail.com>
2020-07-18 15:22:34 -0300

    atualizando rotas

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,11 @@ const routes = express.Router()
 const TodoController = require("./controllers/TodoController")
 
 //rotas para manipulacao de um TODO
-routes.get("/todo", TodoController.index)
-routes.get("/todo/:id", TodoController.show)
-routes.post("/todo", TodoController.store)
-routes.put("/todo/:id", TodoController.update)
-routes.delete("/todo/:id", TodoController.destroy)
+routes.get("/todo/index/", TodoController.index)
+routes.get("/todo/show/:id", TodoController.show)
+routes.post("/todo/store/", TodoController.store)
+routes.put("/todo/update/:id", TodoController.update)
+routes.delete("/todo/destroy/:id", TodoController.destroy)
 
 //exportando rotas para o server.js poder utilizar
 module.exports = routes
